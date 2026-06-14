27b97f017c967b614b301116b246cae7bee8637a
RobinZGit <psl1@mail.ru>
2026-06-14 11:09:27 +0300

    Исправить сборку: импорты Build и Bundle в MainActivity.

--- android/app/src/main/java/com/totalcalendarjs/app/MainActivity.java
+++ android/app/src/main/java/com/totalcalendarjs/app/MainActivity.java
@@ -16,6 +16,8 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.BatteryManager;
+import android.os.Build;
+import android.os.Bundle;
 import android.net.Uri;
 import android.media.AudioAttributes;
 import android.os.Handler;
